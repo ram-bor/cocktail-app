ee34a3e5c8f9e89188097625742067abb568569e
RobertoRamos <robram91@gmail.com>
2021-02-08 10:41:32 -0500

    testing testing

--- server.js
+++ server.js
@@ -14,28 +14,20 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 // support parsing of application/json type post data
 app.use(bodyParser.json());
-//
+app.use(timeout('5s'))
+app.use(haltOnTimedout)
+
 
 const cocktails = require('./routes/cocktail-route');
+
 // Routes
 app.use('/', cocktails);
-app.post('/save', timeout('5s'), bodyParser.json(), haltOnTimedout, function (req, res, next) {
-  savePost(req.body, function (err, id) {
-    if (err) return next(err)
-    if (req.timedout) return
-    res.send('saved as id ' + id)
-  })
-})
 
 function haltOnTimedout (req, res, next) {
   if (!req.timedout) next()
 }
 
-function savePost (post, cb) {
-  setTimeout(function () {
-    cb(null, ((Math.random() * 40000) >>> 0))
-  }, (Math.random() * 7000) >>> 0)
-}
+
 
 // Set up Heroku ports
 app.set('port', process.env.PORT || 5000);
