64be0d0aa6e04cfd4ce85da09110de7cba6ff69b
RobertoRamos <robram91@gmail.com>
2021-02-10 13:41:21 -0500

    tinkering...

--- db/fetch.js
+++ db/fetch.js
@@ -2,11 +2,11 @@
 const fetch = require('node-fetch');
 
 // Connect and retrieve data from database
-const mongoURI = 'mongodb://localhost/cocktails_db';
+// const mongoURI = 'mongodb://localhost/cocktails_db';
 
-fetch(mongoURI)
-  .then(res => res.json())
-  .then(res => {
-    console.log('Success');
-    console.log(res);
-  });
+// fetch(mongoURI)
+//   .then(res => res.json())
+//   .then(res => {
+//     console.log('Success');
+//     console.log(res);
+//   });
--- db/seed.js
+++ db/seed.js
@@ -1,2 +1,1 @@
-const Cocktail = require('../models/cocktail-model');
-
+// const Cocktail = require('../models/cocktail-model');
