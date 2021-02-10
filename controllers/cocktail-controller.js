38fb55a4e09f3228ad6ea8d07624f59514ee7062
RobertoRamos <robram91@gmail.com>
2021-02-10 15:43:22 -0500

    seeding issue

--- controllers/cocktail-controller.js
+++ controllers/cocktail-controller.js
@@ -3,15 +3,8 @@ const Cocktail = require('../models/cocktail-model');
 module.exports = {
   // Display all Cocktails
   showCocktails: (req, resp) => {
-    Cocktail.find({ category: 'cocktail', category: 'ordinary drink' }).then(
-      drink => {
-        resp.json(drink);
-      }
-    );
+    Cocktail.find({}).then(drink => {
+      resp.json(drink);
+    });
   },
-  // showCocktails: (req, resp) => {
-  //   Cocktail.find({}).then(drink => {
-  //     resp.json(drink);
-  //   });
-  // },
 };
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,11 @@
 const Cocktail = require('../models/cocktail-model');
+
+const data = require("./authors.json");
+
+Author.deleteMany({}).then(() => {
+  Author.create(data).then(authors => {
+    console.log(authors);
+    process.exit()
+  });
+});
+
