ca0e8d95c153206892d3dfc762f0317180725e24
RobertoRamos <robram91@gmail.com>
2021-02-11 09:04:37 -0500

    finally added json file

--- db/seed.js
+++ db/seed.js
@@ -1,14 +1,16 @@
-const data = 'mongodb://localhost/cocktails_db';
+const data = require('./cocktails.json');
 const Cocktail = require('../models/cocktail-model');
 
-// console.dir(Cocktail);
+const drinkData = data.map(thing => {
+  const cocktail = {};
+  cocktail.name = thing.drink;
+  cocktail.category = thing.category;
+  cocktail.glass = thing.glass;
+  cocktail.ingredients = thing.ingredients;
+  cocktail.instructions = thing.instructions;
+  return cocktail;
+});
 
-// const drinkData = data.map(thing => {
-//   const cocktail = {};
-//   cocktail.name = thing.name;
-//   cocktail.category = thing.category;
-//   cocktail.glass = thing.glass;
-//   cocktail.ingredients = thing.ingredients;
-//   cocktail.instructions = thing.instructions;
-//   return cocktail;
-// });
+console.log(drinkData);
+
+Cocktail
