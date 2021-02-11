b52e4c03109be13efa92c479502c44be543ca589
RobertoRamos <robram91@gmail.com>
2021-02-10 20:07:45 -0500

    seeding data

--- db/seed.js
+++ db/seed.js
@@ -1,3 +1,17 @@
-const Cocktail = require('../models/cocktail-model');
+const { Seeder } = require('mongo-seeding');
 
-console.log(Cocktail.find());
+// Connect to database property using URI
+const config = {
+  database: 'mongodb://localhost/cocktails_db',
+  dropDatabase: true,
+};
+
+// Instantiate Seeder class
+const seeder = new Seeder(config);
+
+// Seed database with promises
+seeder.import(collections).then(() => {
+  console.log('Success, database seeded!').catch(err => {
+    'Oh no! There was an error.', err;
+  });
+});
