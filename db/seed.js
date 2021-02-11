ce930039da66900695e8ad689c2b9609831be792
RobertoRamos <robram91@gmail.com>
2021-02-11 08:57:31 -0500

    testing seed again

---
+++ db/seed.js
@@ -0,0 +1,14 @@
+const data = 'mongodb://localhost/cocktails_db';
+const Cocktail = require('../models/cocktail-model');
+
+// console.dir(Cocktail);
+
+// const drinkData = data.map(thing => {
+//   const cocktail = {};
+//   cocktail.name = thing.name;
+//   cocktail.category = thing.category;
+//   cocktail.glass = thing.glass;
+//   cocktail.ingredients = thing.ingredients;
+//   cocktail.instructions = thing.instructions;
+//   return cocktail;
+// });
